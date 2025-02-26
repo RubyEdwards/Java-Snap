a2c63865a42b9b9c711bcc740aec22981f69ed5b
Ruby Edwards <ruby.vte@gmail.com>
2025-02-26 10:53:38 +0000

    implements timer for user input

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,20 @@ import main.java.Snap;
 public class Main {
 
     public static void main(String[] args) {
-        CardGame snap = new CardGame("snap");
-        snap.getDeck();
-        snap.dealCard();
-        snap.getDeck();
-        snap.sortDeckInNumberOrder();
-        snap.getDeck();
-        snap.sortDeckIntoSuits();
-        snap.getDeck();
-        snap.shuffleDeck();
-        snap.getDeck();
+        // CardGame snap = new CardGame("snap");
+        // snap.getDeck();
+        // snap.dealCard();
+        // snap.getDeck();
+        // snap.sortDeckInNumberOrder();
+        // snap.getDeck();
+        // snap.sortDeckIntoSuits();
+        // snap.getDeck();
+        // snap.shuffleDeck();
+        // snap.getDeck();
 
-        Snap newSnap = new Snap("snap");
-        newSnap.shuffleDeck();
-        newSnap.play();
+        // Snap newSnap = new Snap("snap");
+        // newSnap.shuffleDeck();
+        // newSnap.play();
 
         Player playerOne = new Player("Ruby");
         Player playerTwo = new Player("Bob");
--- src/main/java/Snap.java
+++ src/main/java/Snap.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Snap extends CardGame {
     private Player playerOne;
     private Player playerTwo;
@@ -12,6 +15,14 @@ public class Snap extends CardGame {
         super(name);
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
+    }
+
+    public Player getPlayerOne() {
+        return playerOne;
+    }
+
+    public Player getPlayerTwo() {
+        return playerTwo;
     }
 
     public void play() {
@@ -62,9 +73,29 @@ public class Snap extends CardGame {
                 }
                 System.out.print("Turn " + count + " (" + currentPlayer.getName() + "): ");
                 Card current = this.dealCard();
+
                 if (current.getSuit() == previous.getSuit()) {
-                    System.out.println(currentPlayer.getName() + " wins! Snap with " + current.getSuit());
-                    break;
+                    String winner = currentPlayer.getName();
+                    TimerTask shutdown = new TimerTask() {
+                        public void run() {
+                            System.out.println(winner + " loses! Snap with " + current.getSuit()
+                                    + " but you ran out of time :(");
+                            System.exit(0);
+                        }
+                    };
+                    Timer timer = new Timer();
+                    timer.schedule(shutdown, 2000);
+                    String snapEntry = System.console().readLine();
+                    if (snapEntry.equals("snap")) {
+                        System.out.println(winner + " wins! Snap with " + current.getSuit());
+                        System.exit(0);
+                        // break;
+                    } else {
+                        System.out.println(winner + " loses! Snap with " + current.getSuit()
+                                + " but you didn't enter snap :(");
+                        System.exit(0);
+                        // break;
+                    }
                 } else {
                     previous = current;
                     input = System.console().readLine();
